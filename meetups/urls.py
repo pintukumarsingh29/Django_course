0c56df19dfd9e9807baa7629c022e35675ecc2e2
pintukumarsingh29 <pintusingh68902@gmail.com>
2021-12-08 09:00:15 +0530

    Finishing the code

--- meetups/urls.py
+++ meetups/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('meetups/', views.index, name='all-meetups'),  # our-domain.com/meetups
-    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail'),  # /meetups/<dynamic-path-segment>
-]
+  path('', views.index, name='all-meetups'),  # our-domain.com/meetups
+  path('<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registration'),
+  path('<slug:meetup_slug>', views.meetup_details, name='meetup-detail'),
 
+]
